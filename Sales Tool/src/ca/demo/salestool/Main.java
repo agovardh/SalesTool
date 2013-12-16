bf75160f1539f485637dcca4a3881b2127c6c632
Abhijit <abhijit.govardhan@lowes.com>
2013-12-15 22:09:38 -0500

    Changed Main print line statements

--- Sales Tool/src/ca/demo/salestool/Main.java
+++ Sales Tool/src/ca/demo/salestool/Main.java
@@ -12,9 +12,12 @@ public class Main {
 	}
 	
 	private static void dispalyGreeting() {
-		System.out.println("Hello Happy Sales People !");
-		System.out.println ("This app Shows Sales Data");
+		System.out.println("Hello Happy Sales People in US !!!");
+		System.out.println ("This Application Shows Sales Data");
 		System.out.println ("-------------------------");
+		
+		
+		
 		
 		System.out.println(" This is second last line of Main");
 		System.out.println(" This is last line of Main");
